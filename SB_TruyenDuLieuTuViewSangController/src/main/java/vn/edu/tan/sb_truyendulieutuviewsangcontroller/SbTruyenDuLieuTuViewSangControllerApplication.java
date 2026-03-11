c155d82d90dc327c6ab893e2f1ddc4cc9eb6c3ff
BuiVuTan <toisesongthatvoichinhminh@gmail.com>
2026-03-11 14:21:56 +0700

    thuc hanh

---
+++ SB_TruyenDuLieuTuViewSangController/src/main/java/vn/edu/tan/sb_truyendulieutuviewsangcontroller/SbTruyenDuLieuTuViewSangControllerApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.tan.sb_truyendulieutuviewsangcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTruyenDuLieuTuViewSangControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbTruyenDuLieuTuViewSangControllerApplication.class, args);
+    }
+
+}
---
+++ SB_TruyenDuLieuTuViewSangController/src/test/java/vn/edu/tan/sb_truyendulieutuviewsangcontroller/SbTruyenDuLieuTuViewSangControllerApplicationTests.java
@@ -0,0 +1,14 @@
+package vn.edu.tan.sb_truyendulieutuviewsangcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTruyenDuLieuTuViewSangControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
