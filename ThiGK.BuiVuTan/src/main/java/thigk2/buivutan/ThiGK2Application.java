e6c497a096cc9e93fb3e36a556da340aed3bf9ea
BuiVuTan <toisesongthatvoichinhminh@gmail.com>
2026-04-24 13:51:32 +0700

    Commit lan thu 1

---
+++ ThiGK.BuiVuTan/src/main/java/thigk2/buivutan/ThiGK2Application.java
@@ -0,0 +1,11 @@
+package thigk2.buivutan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiGK2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ThiGK2Application.class, args);
+    }
+}
---
+++ ThiGK.BuiVuTan/src/test/java/thigk2/buivutan/ThiGK2ApplicationTests.java
@@ -0,0 +1,12 @@
+package thigk2.buivutan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiGK2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
